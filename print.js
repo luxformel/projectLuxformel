cd2d5cadd2ba02942fbbc490448351bf95f226a8
luxformel <damon@luxformel.info>
2023-03-05 20:54:23 +0100

    update36commit7

--- print.js
+++ print.js
@@ -1,6 +1,12 @@
 function printFunc() {
+  // resets Theme
+  //themeStylesheetLink.setAttribute("href", `css/Themes/defaultTheme.css`);
+
+  //sets watermark
   document.getElementById("printWatermark").innerHTML =
     "https://luxformel.info/";
+
+  //prints the window
   window.print();
   //console.log("has been clicked");
 }
